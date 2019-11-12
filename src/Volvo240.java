5cf88c2aaa6abafe79e4915f77e0ce34e7bbda94
Erik <erik.g.eriksson1@gmail.com>
2019-11-12 13:19:32 +0100

    JAVADOC

--- src/Volvo240.java
+++ src/Volvo240.java
@@ -8,12 +8,22 @@ import java.awt.*;
  */
 public class Volvo240 extends Car{
 
+    /**
+     * Value representing how trimmed the Volvo240 car is
+     */
     private final static double trimFactor = 1.25;
 
+    /**
+     * Creates Volvo240
+     */
     public Volvo240(){
         super(4, Color.black, 100, "Volvo240");
     }
-    
+
+    /**
+     * Calculates the speed at which the car accelerates depending on the value of trimFactor and engine power
+     * @return the factor the speed changes at
+     */
     public double speedFactor() {
         return getEnginePower() * 0.01 * trimFactor;
     }
