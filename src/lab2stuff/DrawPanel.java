61e385aef7c53208237f57eaf117fa37482043d1
Ogg <ogg.developer@gmail.com>
2019-11-22 10:29:41 +0100

    Fixed loading resources

--- src/lab2stuff/DrawPanel.java
+++ src/lab2stuff/DrawPanel.java
@@ -34,7 +34,7 @@ public class DrawPanel extends JPanel{
 
             // Rememember to rightclick src New -> Package -> name: pics -> MOVE *.jpg to pics.
             // if you are starting in IntelliJ.
-            volvoImage = ImageIO.read(DrawPanel.class.getResourceAsStream("pics/Volvo240.jpg"));
+            volvoImage = ImageIO.read(DrawPanel.class.getResourceAsStream("/pics/Volvo240.jpg"));
         } catch (IOException ex)
         {
             ex.printStackTrace();
