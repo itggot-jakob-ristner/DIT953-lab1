7ad9fa02489beaca464d70461c0dd6198f9f8021
Erik <erik.g.eriksson1@gmail.com>
2019-12-02 16:33:46 +0100

    Implemented collsision detection

--- src/utilities/CollisionDetector.java
+++ src/utilities/CollisionDetector.java
@@ -4,7 +4,14 @@ public final class CollisionDetector {
 
     private CollisionDetector() { }
 
+
     public static boolean isOutside(Vector pos, Vector size, Vector worldSize) {
+        if (pos.getX() < 0 || pos.getY() < 0) {
+            return true;
+        }
+        if (pos.getX() + size.getX() > worldSize.getX() || pos.getY() + size.getY() > worldSize.getY()) {
+            return true;
+        }
         return false;
     }
 }
