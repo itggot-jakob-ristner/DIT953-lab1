0698c12cc7ca7e35ca18c354f9ca58c76d3179d7
Ogg <ogg.developer@gmail.com>
2019-12-11 13:22:43 +0100

    Added method signatures for Model

--- src/model/Model.java
+++ src/model/Model.java
@@ -1,4 +1,80 @@
 package model;
 
+import model.event.EventListener;
+import model.utilities.Vector;
+import model.vehicles.Saab95;
+import model.vehicles.Scania;
+import model.vehicles.Vehicle;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Model {
+    private static final Vector WORLD_SIZE = new Vector(800, 800);
+    private List<Vehicle> vehicles = new ArrayList<>();
+    public void turboOn() {
+        for (Vehicle v : vehicles) {
+            if (v instanceof Saab95) {
+                ((Saab95) v).setTurboOn();
+            }
+        }
+    }
+
+    public void turboOff() {
+        for (Vehicle v : vehicles) {
+            if (v instanceof Saab95) {
+                ((Saab95) v).setTurboOff();
+            }
+        }
+    }
+
+    public void deactivateBed() {
+        for (Vehicle v : vehicles) {
+            if (v instanceof Scania) {
+                ((Scania) v).deActivateFlatbed();
+                System.out.println(((Scania) v).isFlatbedDeactivated());
+            }
+        }
+    }
+
+    public void activateBed() {
+        for (Vehicle v : vehicles) {
+            if (v instanceof Scania) {
+                ((Scania) v).activateFlatbed();
+                System.out.println(((Scania) v).isFlatbedDeactivated());
+            }
+        }
+    }
+
+    void startAll() {
+        for (Vehicle v : vehicles) {
+            v.startEngine();
+        }
+    }
+
+    void stopAll() {
+        for (Vehicle v : vehicles) {
+            v.stopEngine();
+        }
+    }
+
+    /** Calls the break method for each car once */
+    void brakeAll(double amount) {
+        double brake = amount / 100;
+        for (Vehicle v : vehicles) {
+            v.brake(brake);
+        }
+    }
+
+    /** Calls the gas method for each car once */
+    void gasAll(int amount) {
+        double gas = ((double) amount) / 100;
+        for (Vehicle v : vehicles) {
+            v.gas(gas);
+        }
+    }
+
+    void addEventListener(EventListener eventListener) {
+    }
+
 }
---
+++ src/model/event/EventListener.java
@@ -0,0 +1,5 @@
+package model.event;
+
+public interface EventListener {
+    void onEvent(Event e);
+}
