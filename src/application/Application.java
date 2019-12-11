e14d8b23d36e71463ef619ae9b5ea02d3c549d5b
Erik <erik.g.eriksson1@gmail.com>
2019-12-11 13:10:57 +0100

    Added Application with main

---
+++ src/application/Application.java
@@ -0,0 +1,32 @@
+package application;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Application app = new Application();
+    }
+
+
+    // The delay (ms) corresponds to 20 updates a sec (hz)
+    private final int DELAY = 50;
+
+    private Application() {
+
+
+        new Timer(DELAY, new TimerListener()).start();
+    }
+
+    /* Each step the TimerListener moves all the cars in the list and tells the
+     * view to update its images. Change this method to your needs.
+     * */
+    private class TimerListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+
+            // Update here
+        }
+    }
+}
