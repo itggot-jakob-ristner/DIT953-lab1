3f9ccf8b17a7a2dfd5a343ea8fa260b742bd3755
Erik <erik.g.eriksson1@gmail.com>
2019-12-11 14:06:15 +0100

    Updated constructor in View

--- src/view/View.java
+++ src/view/View.java
@@ -1,17 +1,33 @@
 package view;
 
+import controller.Controller;
 import model.Model;
 import model.event.Event;
 import model.event.EventListener;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class View extends JFrame implements EventListener {
 
     private CanvasView canvas;
 
-    public View(Model model) {
+    public View(Model model, Controller controller) {
         this.canvas = new CanvasView(model);
+    }
+
+    private void initializeFrame(String title, int width, int height) {
+        this.setTitle(title);
+        this.setPreferredSize(new Dimension(width, height));
+        this.setLayout(new FlowLayout(FlowLayout.LEFT, 0, 0));
+
+        this.add(createStatusPanel());
+    }
+
+
+
+    private JPanel createStatusPanel() {
+        new JLabel();
     }
 
     @Override
